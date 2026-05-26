4804c69ed38e0dc326e6db1e8a67c113c6fa78ae
Andre1ka11 <andreika20232030@gmail.com>
2026-05-26 12:21:42 +0300

    hw-5: add missing files from mentor review

--- application/urls.py
+++ application/urls.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found, server_error
+
+handler404 = 'application.urls.handler404_view'
+handler500 = 'application.urls.handler500_view'
+
+
+def handler404_view(request, exception=None):
+    return page_not_found(request, exception, template_name='404.html')
+
+
+def handler500_view(request):
+    return server_error(request, template_name='500.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
